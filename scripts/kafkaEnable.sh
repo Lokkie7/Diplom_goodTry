a587d8a035834385e72005e6403fa0f4959d2f83
Lokkie7 <56259225+Lokkie7@users.noreply.github.com>
2022-06-17 01:41:38 +1000

    Delete scripts directory

--- scripts/kafkaEnable.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if systemctl list-unit-files | grep -E 'kafka' | grep enabled > /dev/null
-then
-    echo "Kafka process is Enabled"
-else
-    echo "Kafka process is Stopped"
-fi
--- scripts/zookeeperProcRun.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if ps -ef | grep kafka | grep -v grep | grep -E 'zookeeper.properties' > /dev/null
-then
-    echo "Zookeeper process is Running"
-else
-    echo "Zookeeper process is Stopped"
-fi
-
