650c97875e073934342c43abeed319ee0c70226a
Elizabeth <56259225+Lokkie7@users.noreply.github.com>
2025-03-27 09:44:46 +0300

    Update variables.tf

--- 2-backet/variables.tf
+++ 2-backet/variables.tf
@@ -1,19 +1,19 @@
 ###cloud vars
 variable "token" {
   type        = string
-  default     = "y0_AgAAAAAbzSe1AATuwQAAAAD9usAbAABO5zMGkutHHINMJJ4_c9N2zXKZ9Q"
+  default     = "..."
   description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
 }
 
 variable "cloud_id" {
   type        = string
-  default     = "b1gg6k1qulpgskn6jnfm"
+  default     = "..."
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
 }
 
 variable "folder_id" {
   type        = string
-  default     = "b1gchoos90euojkta52j"
+  default     = "..."
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
 }
 
