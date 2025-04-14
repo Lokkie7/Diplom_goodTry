0857ee7ab241915c6f8040310f5601bbe975548c
Elizabeth <56259225+Lokkie7@users.noreply.github.com>
2025-04-14 12:32:42 +1000

    Update providers.tf

--- 3-project/providers.tf
+++ 3-project/providers.tf
@@ -22,7 +22,7 @@ backend "s3" {
   }
 }
 provider "yandex" {
-  token     = var.token
+  service_account_key_file = file("~/.authorized_key.json")
   cloud_id  = var.cloud_id
   folder_id = var.folder_id
   zone      = var.default_zone_1
